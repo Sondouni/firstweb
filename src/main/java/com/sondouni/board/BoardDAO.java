bca673d1cfb26e4ab82e230b25637fa8fa598e95
Sondouni <ezicland@gmail.com>
2021-11-11 15:25:56 +0900

    2021-11-11

--- src/main/java/com/sondouni/board/BoardDAO.java
+++ src/main/java/com/sondouni/board/BoardDAO.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BoardDAO {
     public static int updBoard(BoardVO param){
@@ -76,11 +78,55 @@ public class BoardDAO {
         }
         return num;
     }
-    public static List<BoardVO> selBoardList(){
-        return selBoardList(null,null);
+    public static List<BoardVO> selBoardListzx(String key,String value){
+        List<BoardVO> list = new ArrayList();
+        List<String> index_list = new ArrayList<>();
+        index_list.add("ㄱ"); index_list.add("ㄴ"); index_list.add("ㄷ"); index_list.add("ㄹ"); index_list.add("ㅁ"); index_list.add("ㅂ"); index_list.add("ㅅ");
+        index_list.add("ㅇ"); index_list.add("ㅈ"); index_list.add("ㅊ"); index_list.add("ㅋ"); index_list.add("ㅌ"); index_list.add("ㅍ"); index_list.add("ㅎ");
+
+        Map<Integer, String> index_map = new HashMap<>();
+        index_map.put(0, "가"); index_map.put(1, "나"); index_map.put(2, "다"); index_map.put(3, "라"); index_map.put(4, "마"); index_map.put(5, "바"); index_map.put(6, "사");
+        index_map.put(7, "아"); index_map.put(8, "자"); index_map.put(9, "차"); index_map.put(10, "카"); index_map.put(11, "타"); index_map.put(12, "파"); index_map.put(13, "하"); index_map.put(14, "힣");
+
+        String index = value;
+        int num = 0;
+        String whereSQL = "";
+        for( int i = 0; i < index_list.size(); i++ ) { if( index.equals(index_list.get(i)) ) { num = i; break; } }
+        whereSQL = key+">= '" + index_map.get(num) + "' and "+key+" < '" + index_map.get(num+1) + "' ";
+
+        Connection con = null;
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+        String sql =" SELECT * FROM t_board where "+whereSQL;
+        //+" like '%"+value+"%'"
+        try {
+            con = DButils.getCon();
+            pr = con.prepareStatement(sql);
+            rs = pr.executeQuery();
+            while (rs.next()){
+                BoardVO vo = new BoardVO();
+
+                vo.setTitle(rs.getString("title"));
+//                vo.setCtnt(rs.getString("ctnt"));
+                vo.setRdt(rs.getString("rdt"));
+                vo.setWriter(rs.getString("writer"));
+                vo.setIboard(rs.getInt("iboard"));
+                list.add(vo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            DButils.close(con,pr,rs);
+        }
+        return list;
+
     }
     public static List<BoardVO> selBoardList(String key,String value){
         List<BoardVO> list = new ArrayList();
+
+
         Connection con = null;
         PreparedStatement pr = null;
         ResultSet rs = null;
--- src/main/java/com/sondouni/board/BoardListServlet.java
+++ src/main/java/com/sondouni/board/BoardListServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/list")
@@ -16,7 +17,10 @@ public class BoardListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String field_ = req.getParameter("sh");
         String query_ = req.getParameter("shtext");
-
+        List<BoardVO> list;
+        List<String> index_list = new ArrayList<>();
+        index_list.add("ㄱ"); index_list.add("ㄴ"); index_list.add("ㄷ"); index_list.add("ㄹ"); index_list.add("ㅁ"); index_list.add("ㅂ"); index_list.add("ㅅ");
+        index_list.add("ㅇ"); index_list.add("ㅈ"); index_list.add("ㅊ"); index_list.add("ㅋ"); index_list.add("ㅌ"); index_list.add("ㅍ"); index_list.add("ㅎ");
         String field = "title";
         if(field_ != null && !field_.equals(""))
             field = field_;
@@ -24,8 +28,13 @@ public class BoardListServlet extends HttpServlet {
         String query = "";
         if(query_ != null && !query_.equals(""))
             query = query_;
-
-        List<BoardVO> list = BoardDAO.selBoardList(field,query);
+        int num=0;
+        for( int i = 0; i < index_list.size(); i++ ) { if( query.equals(index_list.get(i)) ) { num = 1; break; } }
+        if(num==1){
+            list = BoardDAO.selBoardListzx(field,query);
+        }else {
+            list = BoardDAO.selBoardList(field,query);
+        }
         req.setAttribute("data",list);
         String path = "/WEB-INF/jsp/list.jsp";
         req.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(req,res); // path : 경로 ->여기서는 list.jsp가 되어야한다
